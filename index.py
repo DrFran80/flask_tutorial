1587fd5ce6cc276dfe90c677e9dfa1a3dc7d22ab
DrFran80 <frantraversaro@gmail.com>
2020-01-18 18:11:05 -0300

    first db

--- index.py
+++ index.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from model import db
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return(" Welcome to my new page")
+def welcome():
+        return render_template(
+                "welcome.html",
+                message ="Here's a message to view"
+                )
 
-app.run()
+
+@app.route('/card')
+def card_view():
+        card = db[0]
+        return render_template("card.html", card=card)
